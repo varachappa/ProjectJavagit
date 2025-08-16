8b215ce3418297917bb7580a8aaa1ef55ce286d6
varachappa <79801504+varachappa@users.noreply.github.com>
2025-08-17 05:23:36 +0530

    Second Commit

--- src/test/java/PracticeJava/ProjectJava/Learn.java
+++ src/test/java/PracticeJava/ProjectJava/Learn.java
@@ -5,5 +5,7 @@ import java.util.Scanner;
 public class Learn {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("This is the secon line to be printed");
+		System.out.println("Third line is added.");
 	}
 }
