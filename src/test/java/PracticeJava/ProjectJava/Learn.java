bf64b6ec6aa15c8cacd5cf5ec60f9fd2e0b9cc73
varachappa <79801504+varachappa@users.noreply.github.com>
2025-08-17 05:43:23 +0530

    First Commit in BR-1

--- src/test/java/PracticeJava/ProjectJava/Learn.java
+++ src/test/java/PracticeJava/ProjectJava/Learn.java
@@ -7,6 +7,10 @@ public class Learn {
 		System.out.println("Hello World!");
 		System.out.println("This is the secon line to be printed");
 		System.out.println("Third line is added.");
+		
+		int[] a = {1,2,3,4,5,6,7,8,9,10};
+		for(int x : a)
+		System.out.println(a);
 	}
 }
 // End of the file
